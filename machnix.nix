ceed781e2b404dc73ce87e3e27108276d727fca5
Noah D. Brenowitz <nbren12@gmail.com>
2021-04-04 12:04:35 -0700

    apply david hau's suggestions
    https://discourse.nixos.org/t/python-package-rodeo/12282/5?u=nbren12

--- machnix.nix
+++ machnix.nix
@@ -9,5 +9,12 @@ mach-nix.mkPython {
     cartopy
     tensorflow>=2
     xgcm
+
+    # manually add some of cartopy's deps to make mach-nix aware of their sub-deps
+    sphinx == 3.1.1
+    fiona == 1.8.18
+
+    # add scipy to speed up build since mach-nix will use the wheel now
+    scipy
   '';
 }
